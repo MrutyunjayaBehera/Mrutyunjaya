7818607fdbbbb122682652c4c4c48e48eb2d8ba9
MrutyunjayaBehera <55201679+MrutyunjayaBehera@users.noreply.github.com>
2019-09-11 21:27:51 +0530

    Add files via upload

---
+++ problem 3.3.py
@@ -0,0 +1,8 @@
+import numpy as np
+# Filter Characteristics
+R = 1e3 # 1K ohm resistance
+C = 10e-6 # 10 uF capacitance
+
+# Finding 3 dB bandwidth numerically
+print(np.sqrt(np.roots([1,  13,  46,  -1]))/(2*np.pi*R*C))
+
